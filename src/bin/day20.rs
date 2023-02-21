e1556151b0984381856dde0778416c4702a893f0
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-02-21 16:16:42 +1000

    Solve day20 part1

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -40,22 +40,68 @@ pub fn main() {
 }
 
 /// Processes the AOC 2016 Day 20 input file in the format required by the solver functions.
-/// Returned value is ###.
+/// Returned value is sorted vector of inclusive ranges given in the lines of the input file.
 fn process_input_file(filename: &str) -> Vec<RangeInclusive<u32>> {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let mut ranges = raw_input
+        .lines()
+        .map(|line| line.trim().to_string())
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            line.split('-')
+                .map(|item| item.to_string())
+                .collect::<Vec<String>>()
+        })
+        .map(|split| split[0].parse::<u32>().unwrap()..=split[1].parse::<u32>().unwrap())
+        .collect::<Vec<RangeInclusive<u32>>>();
+    ranges.sort_by(|a, b| a.start().cmp(b.start()));
+    ranges
 }
 
-/// Solves AOC 2016 Day 20 Part 1 // ###
-fn solve_part1(_ranges: &[RangeInclusive<u32>]) -> u32 {
-    unimplemented!();
+/// Solves AOC 2016 Day 20 Part 1 // Determines the lowest value that is not included in the given
+/// ranges.
+fn solve_part1(ranges: &[RangeInclusive<u32>]) -> u32 {
+    find_lowest_value_not_included(ranges).unwrap()
 }
 
 /// Solves AOC 2016 Day 20 Part 2 // ###
 fn solve_part2(_ranges: &[RangeInclusive<u32>]) -> usize {
-    unimplemented!();
+    0
+}
+
+/// Finds the lowest value (u32) that is not included in the given ranges (sorted by start value).
+/// Returns None if the entire range of u32 values is covered.
+fn find_lowest_value_not_included(ranges: &[RangeInclusive<u32>]) -> Option<u32> {
+    let mut highest_end: Option<u32> = None;
+    for r in ranges {
+        if highest_end.is_none() {
+            // Check if there is a gap at the start of the u32 value range
+            if *r.start() > 0 {
+                return Some(0);
+            }
+            highest_end = Some(*r.end());
+        } else {
+            // Check if there is a gap between the highest end value and the range start
+            if *r.start() > highest_end.unwrap()
+                && u32::abs_diff(*r.start(), highest_end.unwrap()) >= 2
+            {
+                break;
+            }
+            // New highest end value has been observed
+            if *r.end() > highest_end.unwrap() {
+                highest_end = Some(*r.end());
+            }
+        }
+    }
+    // Check if there is a gap in u32 value range at the end of the ranges
+    if let Some(value) = highest_end {
+        if value < u32::MAX {
+            return Some(highest_end.unwrap() + 1);
+        }
+    }
+    None
 }
 
 #[cfg(test)]
